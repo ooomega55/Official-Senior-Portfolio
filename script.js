07af49775b774fa7afa3d2de892d8a2851c44dcc
ooomega55 <gvf55@outlook.com>
2024-05-23 14:06:42 -0400

    color swapping process

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-document.getElementById("toggleSwitch").onclick = function() {
-    myFunction()
-  };
-
-  function myFunction() {
-    let color = document.body.style.background;
-    if (color === 'black') {
-      document.body.style.background = "#BB9797";
-    } else {
-      document.body.style.background = "black";
-    }  
-
-  }
-
-  function handleOnChangeEvent(x) {
+function handleOnChangeEvent(x) {
     document.body.style.backgroundImage = "url('" + x + "')";
 }
 
